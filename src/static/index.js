7d32ecb7aba692ada623a71aa549a0e7af05bb8a
gwuiinkim <gwuiinkim@gmail.com>
2019-06-10 23:12:23 +0900

    chat with socket io 2

--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,18 @@
 const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+setTimeout(() => socket.emit("helloGuys"), 4000);
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
